ea8c99945258e63268356eb9951c3635cf680201
rsanies <rsanies9@gmail.com>
2020-11-28 10:37:38 -0500

    path-handling improved with pathlib

--- dyn_model/dyn_model.py
+++ dyn_model/dyn_model.py
@@ -7,6 +7,8 @@ import numpy as np
 from funcs import processDynamic
 from models import DataModel, ModelOcv
 
+from pathlib import Path
+
 # parameters
 cellID = 'A123'     # cell identifier
 numpoles = 1        # number of resistor-capacitor pairs in final model
@@ -14,7 +16,7 @@ temps = [-25, -15, -5, 5, 15, 25, 35, 45]   # temperatures
 mags = [10, 10, 30, 45, 45, 50, 50, 50]     # A123
 
 # read model OCV file, previously computed by runProcessOCV
-modelocv = ModelOcv.load('../ocv_model/modelocv.pickle')
+modelocv = ModelOcv.load(Path(f'./ocv_model/modelocv.pickle'))
 
 # initialize array to store battery cell data
 data = np.zeros(len(mags), dtype=object)
